fd11cd3f5cb1ef56dd3cad4148dbcc09bda12454
NAGARRO\gagansharma <gagan.sharma@nagarro.com>
2018-07-14 15:39:29 +0530

    pushing home page image

--- Frontend/Frontend/ClientApp/components/Home.tsx
+++ Frontend/Frontend/ClientApp/components/Home.tsx
@@ -5,6 +5,7 @@ export default class Home extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return <div>
             <h1>Organ Donation Center!</h1>
+            <img src="https://nhsbtdbe.blob.core.windows.net/umbraco-assets/1383/500586_thevitals_campaignpageheader_aw.jpg" />
             <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum :</p>
             <ul>
                 <li><a href='#'>lorem ipsum lorem ipsum </a> and <a href='#'>lorem ipsum </a>lorem ipsum lorem ipsum lorem ipsum </li>
