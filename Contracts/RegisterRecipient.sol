c2db7c8db02d6db160a42bb367df6034d6ba3800
Shubham Gupta <shubhamg@iiitd.ac.in>
2018-07-14 17:56:56 +0530

    pre eval

--- Contracts/RegisterRecipient.sol
+++ Contracts/RegisterRecipient.sol
@@ -13,18 +13,19 @@ contract RegisterRecipient is RegisterDonor {
 		bool hasRecieved;
 	    uint lat;
 	    uint long;
+	    uint required_organ;
 	}
 
 	Recipient[] public recipients;
-	mapping(uint => uint[]) public required_organs;
+	//mapping(uint => uint[]) public required_organs;
 	//mapping(address => uint) public addressToId;
 
 
 
-function addRecipient(uint[] _requiredOrgans, string _aadharHash, string _bloodGroup,uint _dob,uint _lat,uint _long)  external { //add case where recipient can only be added by a doc
+function addRecipient(uint _requiredOrgan, string _aadharHash, string _bloodGroup,uint _dob,uint _lat,uint _long)  external { //add case where recipient can only be added by a doc
         
-        uint id = recipients.push(Recipient(msg.sender, _aadharHash,_bloodGroup,_dob,true,false,_lat,_long)) - 1;
-        required_organs[id] = _requiredOrgans;
+        uint id = recipients.push(Recipient(msg.sender, _aadharHash,_bloodGroup,_dob,true,false,_lat,_long,_requiredOrgan)) - 1;
+        //required_organs[id] = _requiredOrgans;
 
       
      }
