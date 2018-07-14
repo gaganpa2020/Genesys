5eaa83c64001c32c78b1893eeb6fd1d95290c9cb
Shubham Gupta <shubhamg@iiitd.ac.in>
2018-07-14 17:01:00 +0530

    declareD working

--- Contracts/DeclareD.sol
+++ Contracts/DeclareD.sol
@@ -1,14 +1,17 @@
 pragma solidity ^0.4.24;
 
-import "./RegisterRecipient";
+import "./RegisterRecipient.sol";
 
 
 contract DeclareD is RegisterRecipient {
 
 
-function DeclareDead(){
+function DeclareDead(address _donorAddress,uint _timeOfDeath) external {
 	//require that the person calling this function is a doctor
-	
+	uint donorId = addressToId[_donorAddress];
+	donors[donorId].isAvailable = true;
+	donors[donorId].timeOfDeath = _timeOfDeath;
+	//call matching 
 }
 
 }
--- Contracts/RegisterRecipient.sol
+++ Contracts/RegisterRecipient.sol
@@ -1,6 +1,5 @@
 pragma solidity ^0.4.24;
 
-import "./Ownable.sol";
 import "./RegisterDonor.sol";
 
 contract RegisterRecipient is RegisterDonor {
