97208acc465fc3d8facec599461171ed2d4c5654
Shubham Gupta <shubhamg@iiitd.ac.in>
2018-07-14 16:11:10 +0530

    improved

--- Contracts/RegisterDonor.sol
+++ Contracts/RegisterDonor.sol
@@ -13,6 +13,7 @@ contract RegisterDonor is Ownable {
 		bool optIn; //for having possibilitty to opt out...later
 	    uint lat;
 	    uint long;
+	    uint timeOfDeath;
 	}
 
 	Donor[] public donors;
@@ -22,10 +23,14 @@ contract RegisterDonor is Ownable {
 
 function addDonor(uint[] _pledgedOrgans, string _aadharHash, string _bloodGroup,uint _dob,uint _lat,uint _long)  external {
         
-        uint id = donors.push(Donor(msg.sender, _aadharHash,_bloodGroup,_dob,false,true,_lat,_long)) - 1;
+        uint id = donors.push(Donor(msg.sender, _aadharHash,_bloodGroup,_dob,false,true,_lat,_long,0)) - 1;
         pledged_organs[id] = _pledgedOrgans;
       
      }
+
+function getPledgedOrgans(uint _donorId) public returns(uint[]) {
+	return pledged_organs[_donorId];
+}     
 
 
 }
